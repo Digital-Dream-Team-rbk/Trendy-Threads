844a4b3be9efac9763d4a6d20d7a89463d7f9ba5
Amrou-Alwane <amroualwane1@gmail.com>
2023-05-09 19:08:27 +0200

    done with mvc

--- backend/connection.js
+++ backend/connection.js
@@ -3,6 +3,6 @@ const config=require("../backend/config")
 const connection = mysql.createConnection(config);
 connection.connect((err) => {
   if (err) console.log("Error to connect to database", err);
-  else console.log("Yaaay Database connected!!");
+  else console.log("Yaaay Database connected!");
 });
 module.exports=connection
