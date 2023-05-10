dc6fb74e98f1911994c6d9fe5003f26bb7890b1a
SarhaneGhribi <sarhane.ghribi@gmail.com>
2023-05-10 23:45:45 +0100

    minor fixing

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -4,6 +4,6 @@ const admin=require("../controller/admin.js");
 router.get("/:adminmail",admin.getOneAdmin);
 router.post('/',admin.signUpAdmin);
 router.post('/login',admin.loginAdmin);
-router.put("/id",admin.updateAdmin);
-router.delete("/id",admin.deleteAdmin);
+router.put("/:adminid",admin.updateAdmin);
+router.delete("/:adminid",admin.deleteAdmin);
 module.exports=router;
