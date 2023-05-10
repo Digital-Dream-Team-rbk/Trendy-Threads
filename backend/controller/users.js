7cc77898cfa73617c87b52fa1c52324752b6e004
SarhaneGhribi <sarhane.ghribi@gmail.com>
2023-05-10 19:05:35 +0100

    authentication login sign up working back and front

--- backend/controller/users.js
+++ backend/controller/users.js
@@ -1,6 +1,8 @@
 const {getAllUsers,getOneUser,postOneUser,updateOneUser,deleteOneUser}= require ("../models/users")
 const bcrypt = require ("bcrypt")
-const jwt = require ("jsonwebtoken")
+const jwt = require ("jsonwebtoken");
+const db = require("../config");
+
 const getUsers=(req,res)=>{
     const callback=(err,result)=>{
         if(err) res.status(500).send(err)
@@ -27,7 +29,41 @@ const postOne=async (req,res)=>{
         res.status(500).send(error)
     }
 }
+const login = async (req, res) => {
+    try {
+      const dbUser = await getOneUser(req.body.useremail);
+      if (!dbUser || dbUser.length === 0) {
+        res.status(404).send("Non-existing user");
+        return;
+      }
+  
+      const matchedPassword = await bcrypt.compare(
+        req.body.userpw,
+        dbUser[0].userpw
+      );
+  
+      if (matchedPassword) {
+        const token = jwt.sign(
+          {
+            userId: dbUser[0].userid,
+            userEmail: dbUser[0].useremail
+          },
+          "SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+        );
+  
+        res.status(200).json({
+          token: token,
+          message: "Authentication successful"
+        });
+      } else {
+        res.status(400).send("Wrong password");
+      }
+    } catch (error) {
+      res.status(400).send("Problem with login");
+    }
+  };
 module.exports={
     getUsers,
-    postOne
+    postOne,
+    login
 }
---
+++ trendy/src/components/Signup.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import axios from "axios"
+
+const Signup=()=> {
+  const[name,setName]=useState("")
+  const[password,setPassword]=useState("")
+  const[email,setEmail]=useState("")
+  const handelSubmit=()=>{
+    axios.post("http://localhost:3000/api/users",{
+      username:name,
+      useremail:email,
+      userpw:password
+    })
+    .then(suc=>{console.log(suc)})
+    .catch(err=>console.log(err))
+      }
+      console.log(email)
+  return(
+    <div className='sign'>
+      <h3>Sign up</h3>
+        <input type="text" className='inp' placeholder='User Name'onChange={e=>setName(e.target.value)}/><br />
+        <input type="password" className='inp' placeholder='User Password'onChange={e=>setPassword(e.target.value)}/><br />
+        <input type="email" className='inp' placeholder='User E-mail'onChange={e=>setEmail(e.target.value)}/><br />
+        <input type="button" className='btn' value="submit" onClick={handelSubmit}/>
+    </div>
+  )
+}
+export default Signup
