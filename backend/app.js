ca91c3f5471d9693d4bf114d19066c5c61895dc0
SarhaneGhribi <sarhane.ghribi@gmail.com>
2023-05-11 23:47:47 +0100

    minor changes

--- backend/app.js
+++ backend/app.js
@@ -1,52 +1,22 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
-const cloudinary = require('cloudinary').v2;
-
 const app = express();
 const cors =require ('cors');
 app.use(cors());
-
-cloudinary.config({
-    cloud_name: 'dvrzz6dtg',
-    api_key: '361711739922946',
-    api_secret: '1lxWKsa0rLkhY9mMVt_uEguWrbA'
-  });
-
-
+app.use(fileUpload());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 //create route  for admin
 const adminRoute = require('../backend/routes/admin.js');
 // user routes
 const userRoute = require("../backend/routes/users.js")
 // Import products route
 const productsRoute = require('../backend/routes/product.js'); 
-
-
-
-app.use(fileUpload());
-
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
 //use path for controller methodes
 app.use('/api/admin',adminRoute);
 app.use("/api/users",userRoute);
 app.use('/api/products', productsRoute); 
 
-
-app.post('/upload', (req, res) => {
-    const file = req.files.file;
-    
-    // upload file to Cloudinary
-    cloudinary.uploader.upload(file.tempFilePath, (err, result) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send(err);
-      } else {
-        // send URL of uploaded image back to client
-        res.json({ url: result.secure_url });
-      }
-    });
-  });
 
 
 
--- backend/models/users.js
+++ backend/models/users.js
@@ -1,10 +1,13 @@
+// requiring the connection to link database 
 const connection= require("../connection")
+//query function to get all users from the users table
 const getAllUsers=(callback)=>{
     const sql="SELECT * FROM users"
     connection.query(sql,(err,result)=>{
         callback(err,result)
     })
 }
+//query function to get one user from the users table using his email
 const getOneUser=async (useremail)=>{
     const sql=`SELECT * FROM users where useremail="${useremail}"`
     return new Promise((resolve, reject) => {
@@ -17,6 +20,7 @@ const getOneUser=async (useremail)=>{
         });
       })
 }
+//query function to post a new user to the users table
 const postOneUser= async (user)=>{
     const sql=`INSERT INTO users set ?`
     return new Promise((resolve, reject) => {
@@ -29,6 +33,7 @@ const postOneUser= async (user)=>{
         });
       });
 }
+//query function to update one user from the users table
 const updateOneUser=(name,update)=>{
     const sql= `update users set ? where username="${name}"`
     return new Promise((resolve, reject) => {
@@ -37,7 +42,10 @@ const updateOneUser=(name,update)=>{
         reject(err)
       }
       else resolve(result)
-})})}
+}) 
+})
+}
+//query function to delete one user from the users table
 const deleteOneUser=(mail)=>{
     const sql= `delete from users where useremail="${mail}"`
     return new Promise((resolve, reject) => {
@@ -48,6 +56,7 @@ const deleteOneUser=(mail)=>{
       else resolve(result)
     })
 })}
+//exporting all of the function to use them in users.js in the controller folder
 module.exports={
     getAllUsers,
     getOneUser,
