6de71df795d50a0d86860b7b2f2b1a2a6d9f6187
Amrou-Alwane <amroualwane1@gmail.com>
2023-05-11 17:29:47 +0200

    progressing

--- trendy/src/components/admin/HomeAdmin.jsx
+++ trendy/src/components/admin/HomeAdmin.jsx
@@ -1,7 +1,34 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 function HomeAdmin() {
+  const navigate = useNavigate();
+  //data of users
+  const [users,setUsers]=useState([]);
+  //data of products
+  const[products,setProducts]=useState([])
+  //fetch the data from the API
+  const fetchData = () => {
+    return axios.get('http://localhost:3000/api/products/all')
+      .then((res) => {
+        setProducts(res.data)
+        console.log(products)
+        return axios.get('http://localhost:3000/api/users');
+      })
+      .then((res) => {
+        setUsers(res.data);
+        console.log(users)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    fetchData()
+    console.log(users)
+  }, []);
   
+
   return (
 
     <h1>welcome to the home page</h1>
